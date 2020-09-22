4e80d50151795c80c8e39abb420e215110e3ea43
RAMZI MARESH AL-FAIZE <47695617+RamziMaresh@users.noreply.github.com>
2020-09-22 20:42:18 +0800

    Create Exer11_reversSttring.cpp

---
+++ Exer11_reversSttring.cpp
@@ -0,0 +1,29 @@
+/*Write a program that reverses a string and prints it on the screen.*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+	char buffer[10];
+	char reversedbuffer[10];
+	cout<<"Enter a string to be reversed :"<<endl;
+	cin>>buffer;
+
+	for(int i=0; i<= 10;i++){
+	 reversedbuffer[i]=buffer[10-i];
+	 }
+
+
+	cout<<"reversed string :";
+
+	for(int u=0;u<=10;u++){
+	  cout<<reversedbuffer[u];
+	  }
+
+	cout <<"\n";
+
+    return 0;
+}
